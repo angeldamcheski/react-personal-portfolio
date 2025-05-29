6f2ba952de0cab3fd664643ded5e30784b132273
angeldamcheski <damcheskiangel911@gmail.com>
2025-05-29 20:03:07 +0200

    Fade animation added

--- src/components/SuccessBanner.jsx
+++ src/components/SuccessBanner.jsx
@@ -9,7 +9,7 @@ export const SuccessBanner = ({ message, show, onClose }) => {
   }, [show, onClose]);
   if (!show) return null;
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-green-600 text-white text-sm py-0 px-4 text-center z-50 shadow-md">
+    <div className="fixed bottom-0 left-0 w-full bg-green-600 text-white text-sm py-0 px-4 text-center z-50 shadow-md  animate-fade-in">
         <div className="flex justify-center items-center gap-2">
             <svg className="w-4 h-4" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7"></path>
